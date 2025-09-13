3cd3f0747a5e3de5aac49d69ad946d00e0754b61
drgn701 <r.ch.8.16.nd.em@gmail.com>
2025-09-13 17:26:54 +0300

    Add f-strings.py

---
+++ f-strings.py
@@ -0,0 +1,20 @@
+age = 20
+python_version = 3.13
+print(f'Your age is {age}')
+print(f'python_version ={python_version}')
+number = 20
+print(f'{number:_>8}')
+
+x = 20.123345678
+print(f'{x:.5f}')
+
+year = 2025
+month = 9
+day = 5
+print(f'{day:0>2}.{month:0>2}.{year}')
+
+p = 20.123
+print(f'{p:.1f}%')
+
+m = 100_000_000
+print(f'{m:,}')
